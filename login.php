451418fe8c296daab734bdf1b3ea856b802ca878
caoshengyu1992 <51437566+caoshengyu1992@users.noreply.github.com>
2020-09-08 10:14:22 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Happy Puppy</title>
+  </head>
+  <body>
+    <h2>Login Page</h2>
+    <a href="site.php">Click here to go back </a>
+      <br/><br/>
+      <form action="checklogin.php" method="POST">
+        Enter username: <input type="text" name="username" required="required"/> <br/>
+        Enter password:<input type="password" name="password" required="required"/> <br/>
+      </form>
+  </body>
+</html>
---
+++ site.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<h2>welcome to Happy Puppy.</h2>
+  <?php
+  ?>
+  <a href="login.php">Click here to login </a>
+    <a href="register.php">Click here to register </a>
+  </body>
+</html>
